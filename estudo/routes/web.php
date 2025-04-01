189c6907ca945f466182cad058b75ebb9e584b7f
Gabrielle Fernandes <gabfernandes036@gmail.com>
2025-04-01 15:28:07 -0300

    Rotas

--- estudo/routes/web.php
+++ estudo/routes/web.php
@@ -9,3 +9,17 @@ Route::get('/', function () {
 Route::get('/empresa', function() {
     return view('empresa');
 });
+
+Route::any('/any', function() {
+    return "Permite todo tipo de acesso http (put, delete, get, post)";
+});
+
+Route::match(['get', 'post'], '/match', function() {
+    return "Permite apenas acessos definidos";
+});
+
+Route::get('/produto/{id}', function($id) {
+    return "O id do produto é".$id;
+});
+
+Route::redirect('/sobre', '/empresa');
